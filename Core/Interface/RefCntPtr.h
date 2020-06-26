f23b7f25d054c4dd4be73542ad1ae51ce5a2ef24
juteman <hhjuteman@gmail.com>
2020-06-26 11:23:05 +0800

    引用计数接口实现
    Signed-off-by: juteman <hhjuteman@gmail.com>

--- Core/Interface/RefCntPtr.h
+++ Core/Interface/RefCntPtr.h
@@ -21,12 +21,13 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 #pragma once
 #ifndef HAWL_REFCNTPTR_H
 #  define HAWL_REFCNTPTR_H
 #  include "HIID.h"
 #  include "coreafx.h"
-
+#  include <type_traits>
 namespace Hawl {
 class IObject
 {
@@ -50,6 +51,8 @@ public:
 };
 
 /// 引用计数智能指针的实现
+/// @warning 此引用计数接口还未经过测试，使用有风险
+/// @note RefCntPtr 参数必须包含IObject
 template<typename T>
 class RefCntPtr
 {
@@ -63,7 +66,144 @@ public:
       m_pObject->AddRef();
   }
 
+  RefCntPtr(T* pObject, const HIID& IID) noexcept
+    : m_pObject(pObject)
+  {
+    if (pObject)
+      pObject->QueryInterface(IID, reinterpret_cast<IObject**>(&m_pObject));
+  }
+
+  RefCntPtr(const RefCntPtr& AutoPtr) noexcept
+    : m_pObject{ AutoPtr.m_pObject }
+  {
+    if (m_pObject)
+      m_pObject->AddRef();
+  }
+
+  /// 继承类型指针不会增加引用计数
+  template<typename DerivedType,
+           typename = typename std::enable_if<
+             std::is_base_of<T, DerivedType>::value>::type>
+  RefCntPtr(RefCntPtr<DerivedType>&& AutoPtr) noexcept
+    : m_pObject{ std::move(AutoPtr.m_pObject) }
+  {
+    AutoPtr.m_pObject = nullptr;
+  }
+
+  ~RefCntPtr() { Release(); }
+
+  void swap(RefCntPtr& AutoPtr) noexcept
+  {
+    std::swap(m_pObject, AutoPtr.m_pObject);
+  }
+
+  void Attach(T* pObject) noexcept
+  {
+    Release();
+    m_pObject = pObject;
+  }
+
+  T* Detach() noexcept
+  {
+    T* pObject = m_pObject;
+    m_pObject  = nullptr;
+    return pObject;
+  }
+
+  void Release() noexcept
+  {
+    if (m_pObject) {
+      m_pObject->Release();
+      m_pObject = nullptr;
+    }
+  }
+
+  RefCntPtr& operator=(T* pObject) noexcept
+  {
+    if (m_pObject != pObject) {
+      if (m_pObject)
+        m_pObject->Release();
+      m_pObject = pObject;
+      if (m_pObject)
+        m_pObject->AddRef();
+    }
+    return *this;
+  }
+
+  RefCntPtr& operator=(const RefCntPtr& AutoPtr) noexcept
+  {
+    return *this = AutoPtr.m_pObject;
+  }
+
+  template<typename DerivedType,
+           typename = typename std::enable_if<
+             std::is_base_of<T, DerivedType>::value>::type>
+  RefCntPtr& operator=(const RefCntPtr<DerivedType>& AutoPtr) noexcept
+  {
+    return *this = static_cast<T*>(AutoPtr.m_pObject);
+  }
+
+  RefCntPtr& operator=(RefCntPtr&& AutoPtr) noexcept
+  {
+    if (m_pObject != AutoPtr.m_pObject)
+      Attach(AutoPtr.Detach());
+
+    return *this;
+  }
+
+  template<typename DerivedType,
+           typename = typename std::enable_if<
+             std::is_base_of<T, DerivedType>::value>::type>
+  RefCntPtr& operator=(RefCntPtr<DerivedType>&& AutoPtr) noexcept
+  {
+    if (m_pObject != AutoPtr.m_pObject)
+      Attach(AutoPtr.Detach());
+
+    return *this;
+  }
+
+  bool operator!() const noexcept { return m_pObject == nullptr; }
+
+  operator bool() const noexcept { return m_pObject != nullptr; }
+
+  bool operator==(const RefCntPtr& Ptr) const noexcept
+  {
+    return m_pObject == Ptr.m_pObject;
+  }
+
+  bool operator!=(const RefCntPtr& Ptr) const noexcept
+  {
+    return m_pObject != Ptr.m_pObject;
+  }
+
+  bool operator<(const RefCntPtr& Ptr) const noexcept
+  {
+    return static_cast<const T*>(*this) < static_cast<const T*>(Ptr);
+  }
+
+  T& operator*() noexcept { return *m_pObject; }
+
+  const T& operator*() const noexcept { return *m_pObject; }
+
+  T*       RawPtr() noexcept { return m_pObject; }
+  const T* RawPtr() const noexcept { return m_pObject; }
+
+  operator T*() noexcept { return RawPtr(); }
+  operator const T*() const noexcept { return RawPtr(); }
+
+  T*       operator->() noexcept { return m_pObject; }
+  const T* operator->() const noexcept { return m_pObject; }
+
+  template<typename InterfaceType>
+  RefCntPtr<InterfaceType> Cast(const HIID& IID)
+  {
+    return RefCntAutoPtr<InterfaceType>{ m_pObject, IID };
+  }
+
 private:
+  template<typename OtherType>
+  friend class RefCntPtr;
+
   T* m_pObject = nullptr;
 };
 }
